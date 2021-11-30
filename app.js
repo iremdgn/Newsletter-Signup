b902e52460f9cb329e1f87f8a1409312cdde72cc
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2021-11-30 22:56:08 +0300

    update

--- app.js
+++ app.js
@@ -1,19 +1,65 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 app.get('/', function (req, res) {
 
     res.sendFile(__dirname + '/signup.html');
+
 });
 
+app.post('/', function (req, res) {
 
+    const firsName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-app.listen(3000 , function () {
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firsName,
+                    LNAME: lastName,
+                }
+            }
+        ]        
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us20.api.mailchimp.com/3.0/lists/0513c9ea5a"
+    const options = {
+        method: "POST",
+        auth: "irem:cccbc1cdb6db60a09e3d832a5c4cd41b-us20"
+    }
+
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.listen(3000, function () {
     console.log('server started on port 3000');
-})
+});
+
+//api key 
+//cccbc1cdb6db60a09e3d832a5c4cd41b-us20
+
+//audience id
+//0513c9ea5a
