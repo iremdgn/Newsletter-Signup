3c3bae9875bfa18572d898994b0bd1f583472c08
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2021-12-01 17:44:21 +0300

    update app.js

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.post('/', function (req, res) {
 });
 
 
-app.listen( process.env.POST || 3000, function () {
+app.listen( process.env.PORT || 3000, function () {
     console.log('server started on port 3000');
 });
 
